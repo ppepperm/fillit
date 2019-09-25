4cff7904d0e61cf28ca0b00500418f94649fddd9
Peppermint Peppermint <ppepperm@ga-q1.21-school.ru>
2019-09-25 18:41:24 +0300

    done_input_but_not_leaks

--- fillit.h
+++ fillit.h
@@ -1,24 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ppepperm <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/04 15:14:09 by ppepperm          #+#    #+#             */
-/*   Updated: 2019/09/18 17:36:36 by ppepperm         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+//
+// Created by Peppermint Peppermint on 25/09/2019.
+//
 
-#ifndef FILLIT_H
-# define FILLIT_H
-# include "libft.h"
+#ifndef FILLIT_FILLIT_H
+#define FILLIT_FILLIT_H
 
-typedef struct	s_tetrm
-{
-	int		*data;
-	char	c;
-	struct	s_tetrm *next;
-}				t_tet;
-
-#endif
+#endif //FILLIT_FILLIT_H
